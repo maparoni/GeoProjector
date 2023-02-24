53823aaeb581716675129c5d792b3237df211663
Adrian Schoenig <adrian@schoenig.me>
2023-02-24 21:48:41 +1100

    Add short license text to source files

--- Examples/Cassini/CassiniApp.swift
+++ Examples/Cassini/CassiniApp.swift
@@ -4,6 +4,23 @@
 //
 //  Created by Adrian Schönig on 10/12/2022.
 //
+// GeoProjector - Native Swift library for drawing map projections
+// Copyright (C) 2022 Corporoni Pty Ltd
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+// USA
 
 import SwiftUI
 
