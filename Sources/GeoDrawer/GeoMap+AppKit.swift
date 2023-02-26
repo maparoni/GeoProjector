6ed1d4d571b5c41b666e4ab9e8cc8b34baff2ac8
Adrian Schoenig <adrian@schoenig.me>
2023-02-26 11:22:14 +1100

    Various tweaks to improve usage
    - Expose more methods publicly
    - Compatibility with Mac Catalyst

--- Sources/GeoDrawer/GeoMap+AppKit.swift
+++ Sources/GeoDrawer/GeoMap+AppKit.swift
@@ -22,7 +22,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
 // USA
 
-#if canImport(AppKit)
+#if canImport(AppKit) && !targetEnvironment(macCatalyst)
 import AppKit
 import SwiftUI
 
