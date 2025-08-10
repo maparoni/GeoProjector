83f89b72eec33c23f838e74e50c8b05812fa739d
Adrian Schoenig <adrian@schoenig.me>
2025-08-10 15:51:45 +1000

    Convert all tests to Swift Testing

--- Tests/GeoProjectorTests/ProjectionTests.swift
+++ Tests/GeoProjectorTests/ProjectionTests.swift
@@ -1,36 +1,37 @@
-import XCTest
+#if canImport(Testing)
+import Testing
 
 import GeoJSONKit
 @testable import GeoProjector
 
-final class ProjectionTests: XCTestCase {
+struct ProjectionTests {
 
-  func testFlatSquare() throws {
+  @Test func flatSquare() throws {
     let topLeft = GeoJSON.Position(latitude: 90, longitude: -180)
-    XCTAssertEqual(Projections.Equirectangular().point(for: topLeft, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0, .init(x: 0, y: 0))
+    #expect(Projections.Equirectangular().point(for: topLeft, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0 == .init(x: 0, y: 0))
 
     let topRight = GeoJSON.Position(latitude: 90, longitude: 180)
-    XCTAssertEqual(Projections.Equirectangular().point(for: topRight, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0, .init(x: 200, y: 0))
+    #expect(Projections.Equirectangular().point(for: topRight, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0 == .init(x: 200, y: 0))
 
     let bottomLeft = GeoJSON.Position(latitude: -90, longitude: -180)
-    XCTAssertEqual(Projections.Equirectangular().point(for: bottomLeft, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0, .init(x: 0, y: 100))
+    #expect(Projections.Equirectangular().point(for: bottomLeft, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0 == .init(x: 0, y: 100))
 
     let bottomRight = GeoJSON.Position(latitude: -90, longitude: 180)
-    XCTAssertEqual(Projections.Equirectangular().point(for: bottomRight, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0, .init(x: 200, y: 100))
+    #expect(Projections.Equirectangular().point(for: bottomRight, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0 == .init(x: 200, y: 100))
 
     let zeroZero = GeoJSON.Position(latitude: 0, longitude: 0)
-    XCTAssertEqual(Projections.Equirectangular().point(for: zeroZero, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0, .init(x: 100, y: 50))
+    #expect(Projections.Equirectangular().point(for: zeroZero, size: .init(width: 200, height: 100), coordinateSystem: .topLeft)?.0 == .init(x: 100, y: 50))
   }
 
-  func testOrthographic() throws {
+  @Test func orthographic() throws {
     let zeroZero = GeoJSON.Position(latitude: 0, longitude: 0)
-    XCTAssertEqual(Projections.Orthographic().point(for: zeroZero, size: .init(width: 100, height: 100), coordinateSystem: .topLeft)?.0, .init(x: 50, y: 50))
+    #expect(Projections.Orthographic().point(for: zeroZero, size: .init(width: 100, height: 100), coordinateSystem: .topLeft)?.0 == .init(x: 50, y: 50))
 
     let easternOrthographic = Projections.Orthographic(reference: .init(latitude: 0, longitude: 100))
     let sydney = GeoJSON.Position(latitude: -33.8, longitude: 151.3)
     let projected = easternOrthographic.point(for: sydney, size: .init(width: 100, height: 100), coordinateSystem: .topLeft)?.0
-    XCTAssertEqual(projected?.x ?? 0, 82.4, accuracy: 0.1)
-    XCTAssertEqual(projected?.y ?? 0, 77.8, accuracy: 0.1)
+    #expect(abs((projected?.x ?? 0) - 82.4) < 0.1)
+    #expect(abs((projected?.y ?? 0) - 77.8) < 0.1)
   }
 
 }
@@ -41,3 +42,5 @@ extension Projection {
     return self.point(for: point, size: size, zoomTo: zoomTo, insets: insets, coordinateSystem: coordinateSystem)
   }
 }
+
+#endif
--- Tests/GeoProjectorTests/TranslationTests.swift
+++ Tests/GeoProjectorTests/TranslationTests.swift
@@ -5,32 +5,35 @@
 //  Created by Adrian Schönig on 9/12/2022.
 //
 
-import XCTest
+#if canImport(Testing)
+import Testing
 
 @testable import GeoProjector
 
-final class TranslationTests: XCTestCase {
-  func testEquirectangularInSquare() throws {
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: 0, y: 0), to: .init(width: 100, height: 100), coordinateSystem: .topLeft), .init(x: 50, y: 50))
+struct TranslationTests {
+  @Test func equirectangularInSquare() throws {
+    #expect(Projections.Equirectangular().translate(Point(x: 0, y: 0), to: .init(width: 100, height: 100), coordinateSystem: .topLeft) == .init(x: 50, y: 50))
 
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: -1 * .pi, y: 0.5 * .pi), to: .init(width: 100, height: 100), coordinateSystem: .topLeft), .init(x: 0, y: 25))
+    #expect(Projections.Equirectangular().translate(Point(x: -1 * .pi, y: 0.5 * .pi), to: .init(width: 100, height: 100), coordinateSystem: .topLeft) == .init(x: 0, y: 25))
 
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: .pi, y: 0.5 * .pi), to: .init(width: 100, height: 100), coordinateSystem: .topLeft), .init(x: 100, y: 25))
+    #expect(Projections.Equirectangular().translate(Point(x: .pi, y: 0.5 * .pi), to: .init(width: 100, height: 100), coordinateSystem: .topLeft) == .init(x: 100, y: 25))
 
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: .pi, y: -0.5 * .pi), to: .init(width: 100, height: 100), coordinateSystem: .topLeft), .init(x: 100, y: 75))
+    #expect(Projections.Equirectangular().translate(Point(x: .pi, y: -0.5 * .pi), to: .init(width: 100, height: 100), coordinateSystem: .topLeft) == .init(x: 100, y: 75))
 
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: -1 * .pi, y: -0.5 * .pi), to: .init(width: 100, height: 100), coordinateSystem: .topLeft), .init(x: 0, y: 75))
+    #expect(Projections.Equirectangular().translate(Point(x: -1 * .pi, y: -0.5 * .pi), to: .init(width: 100, height: 100), coordinateSystem: .topLeft) == .init(x: 0, y: 75))
   }
   
-  func testEquirectangularInWideStripe() throws {
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: 0, y: 0), to: .init(width: 400, height: 100), coordinateSystem: .topLeft), .init(x: 200, y: 50))
+  @Test func equirectangularInWideStripe() throws {
+    #expect(Projections.Equirectangular().translate(Point(x: 0, y: 0), to: .init(width: 400, height: 100), coordinateSystem: .topLeft) == .init(x: 200, y: 50))
 
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: -1 * .pi, y: 0.5 * .pi), to: .init(width: 400, height: 100), coordinateSystem: .topLeft), .init(x: 100, y: 0))
+    #expect(Projections.Equirectangular().translate(Point(x: -1 * .pi, y: 0.5 * .pi), to: .init(width: 400, height: 100), coordinateSystem: .topLeft) == .init(x: 100, y: 0))
 
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: .pi, y: 0.5 * .pi), to: .init(width: 400, height: 100), coordinateSystem: .topLeft), .init(x: 300, y: 0))
+    #expect(Projections.Equirectangular().translate(Point(x: .pi, y: 0.5 * .pi), to: .init(width: 400, height: 100), coordinateSystem: .topLeft) == .init(x: 300, y: 0))
 
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: .pi, y: -0.5 * .pi), to: .init(width: 400, height: 100), coordinateSystem: .topLeft), .init(x: 300, y: 100))
+    #expect(Projections.Equirectangular().translate(Point(x: .pi, y: -0.5 * .pi), to: .init(width: 400, height: 100), coordinateSystem: .topLeft) == .init(x: 300, y: 100))
 
-    XCTAssertEqual(Projections.Equirectangular().translate(Point(x: -1 * .pi, y: -0.5 * .pi), to: .init(width: 400, height: 100), coordinateSystem: .topLeft), .init(x: 100, y: 100))
+    #expect(Projections.Equirectangular().translate(Point(x: -1 * .pi, y: -0.5 * .pi), to: .init(width: 400, height: 100), coordinateSystem: .topLeft) == .init(x: 100, y: 100))
   }
 }
+
+#endif
